709fc69096419596e2ea673f33f9d6779276a40f
Jacques Fortier <jacquesf@gmail.com>
2011-03-26 00:19:31 -0400

    Initial commit

---
+++ cobs.h
@@ -0,0 +1,10 @@
+#ifndef COBS_H
+#define COBS_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+size_t cobs_encode(const uint8_t * restrict input, size_t length, uint8_t * restrict output);
+size_t cobs_decode(const uint8_t * restrict input, size_t length, uint8_t * restrict output);
+
+#endif
