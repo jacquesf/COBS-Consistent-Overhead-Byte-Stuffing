d87effbc1e49e88d0524d36b4e3d41657f4fcb41
Tomi Virtanen <tomi.virtanen@grandcentrix.net>
2022-02-23 19:37:51 +0100

    Add in-place variant of cobs_decode
    Function cobs_decode_inplace decodes COBS framed data in-place.

--- cobs.h
+++ cobs.h
@@ -10,5 +10,6 @@
 
 size_t cobs_encode(const uint8_t * restrict input, size_t length, uint8_t * restrict output);
 size_t cobs_decode(const uint8_t * restrict input, size_t length, uint8_t * restrict output);
+size_t cobs_decode_inplace(uint8_t * restrict data, size_t max_length);
 
 #endif
