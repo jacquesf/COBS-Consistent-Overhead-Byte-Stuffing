72d895ba220aa0c89f00e925426771bea8beabca
Michael Zimmermann <michael.zimmermann@grandcentrix.net>
2023-07-05 19:36:08 +0200

    tests: use more constness

--- tests/cobs/src/main.c
+++ tests/cobs/src/main.c
@@ -10,20 +10,20 @@
 #include <zephyr/ztest.h>
 #include <cobs.h>
 
-static void roundtrip_test_runner(const void *input, size_t length)
+static void roundtrip_test_runner(const void *input, const size_t length)
 {
-	size_t encoded_buffer_length = length + length / 254 + 2;
-	uint8_t *encoded_buffer = malloc(encoded_buffer_length);
-	uint8_t *decoded_buffer = malloc(length + 1);
+	const size_t encoded_buffer_length = length + length / 254 + 2;
+	uint8_t *const encoded_buffer = malloc(encoded_buffer_length);
+	uint8_t *const decoded_buffer = malloc(length + 1);
 	memset(encoded_buffer, 0xAB, encoded_buffer_length);
 	decoded_buffer[length] = 0xAB;
 
-	size_t encoded_length = cobs_encode(input, length, encoded_buffer);
+	const size_t encoded_length = cobs_encode(input, length, encoded_buffer);
 	zassert_true(encoded_length <= (length + length / 254 + 1));
 	zassert_equal(encoded_buffer[encoded_length], 0xAB);
 	zassert_equal(encoded_buffer[encoded_buffer_length - 1], 0xAB);
 
-	size_t decoded_length = cobs_decode(encoded_buffer, encoded_length, decoded_buffer);
+	const size_t decoded_length = cobs_decode(encoded_buffer, encoded_length, decoded_buffer);
 	zassert_equal(decoded_length, length);
 	zassert_mem_equal(decoded_buffer, input, length);
 	zassert_equal(decoded_buffer[length], 0xAB);
@@ -35,11 +35,11 @@ static void roundtrip_test_runner(const void *input, size_t length)
 
 ZTEST(lib_cobs_test, test_single_null)
 {
-	uint8_t buffer[] = {0};
+	static const uint8_t buffer[] = {0};
 	uint8_t encoded_buffer[3] = {0xAB, 0xAB, 0xAB};
-	uint8_t expected_buffer[3] = {1, 1, 0xAB};
+	static const uint8_t expected_buffer[3] = {1, 1, 0xAB};
 
-	size_t encoded_length = cobs_encode(buffer, sizeof(buffer), encoded_buffer);
+	const size_t encoded_length = cobs_encode(buffer, sizeof(buffer), encoded_buffer);
 
 	zassert_equal(encoded_length, 2);
 	zassert_mem_equal(encoded_buffer, expected_buffer, sizeof(encoded_buffer));
@@ -47,11 +47,11 @@ ZTEST(lib_cobs_test, test_single_null)
 
 ZTEST(lib_cobs_test, test_hex1)
 {
-	uint8_t buffer[] = {1};
+	static const uint8_t buffer[] = {1};
 	uint8_t encoded_buffer[3] = {0xAB, 0xAB, 0xAB};
-	uint8_t expected_buffer[3] = {2, 1, 0xAB};
+	static const uint8_t expected_buffer[3] = {2, 1, 0xAB};
 
-	size_t encoded_length = cobs_encode(buffer, sizeof(buffer), encoded_buffer);
+	const size_t encoded_length = cobs_encode(buffer, sizeof(buffer), encoded_buffer);
 
 	zassert_equal(encoded_length, 2);
 	zassert_mem_equal(encoded_buffer, expected_buffer, sizeof(encoded_buffer));
@@ -86,35 +86,35 @@ ZTEST(lib_cobs_test, test_255_bytes_null_end)
 
 ZTEST(lib_cobs_test, test_hex1_rt)
 {
-	uint8_t buffer[] = {1};
+	const uint8_t buffer[] = {1};
 
 	roundtrip_test_runner(buffer, sizeof(buffer));
 }
 
 ZTEST(lib_cobs_test, test_single_null_rt)
 {
-	uint8_t buffer[] = {0};
+	const uint8_t buffer[] = {0};
 
 	roundtrip_test_runner(buffer, sizeof(buffer));
 }
 
 ZTEST(lib_cobs_test, test_two_nulls_rt)
 {
-	uint8_t buffer[] = {0, 0};
+	const uint8_t buffer[] = {0, 0};
 
 	roundtrip_test_runner(buffer, sizeof(buffer));
 }
 
 ZTEST(lib_cobs_test, test_null_one_null_rt)
 {
-	uint8_t buffer[] = {0, 1, 0};
+	const uint8_t buffer[] = {0, 1, 0};
 
 	roundtrip_test_runner(buffer, sizeof(buffer));
 }
 
 ZTEST(lib_cobs_test, test_null_two_null_one_rt)
 {
-	uint8_t buffer[] = {0, 2, 0, 1};
+	const uint8_t buffer[] = {0, 2, 0, 1};
 
 	roundtrip_test_runner(buffer, sizeof(buffer));
 }
